2bc4b815f397699aeb9ce750a93065098561b9fa
Agustin Martinez <abel.am91@gmail.com>
2020-11-12 16:11:48 -0300

    Agregar archivos de proyecto.

---
+++ FormularioFixture/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace FormularioFixture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(423, 383);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button btnResta;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Button btnN7;
+        private System.Windows.Forms.Button btnN4;
+        private System.Windows.Forms.Button btnN1;
+        private System.Windows.Forms.Button btnN2;
+        private System.Windows.Forms.Button btnN5;
+        private System.Windows.Forms.Button btnN8;
+        private System.Windows.Forms.Button btnN3;
+        private System.Windows.Forms.Button btnN6;
+        private System.Windows.Forms.Button btnN9;
+        private System.Windows.Forms.Button btnIgual;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnBT;
+        private System.Windows.Forms.Button btnPunto;
+        private System.Windows.Forms.Button btnN0;
+        private System.Windows.Forms.TextBox tbxPantalla;
+    }
+}
+
---
+++ FormularioFixture/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormularioFixture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnN0_Click(object sender, EventArgs e)
+        {
+            tbxPantalla.Text = tbxPantalla.Text + "0";
+
+
+        }
+        private void btnN1_Click(object sender, EventArgs e)
+        {
+            tbxPantalla.Text = tbxPantalla.Text + "1";
+
+
+        }
+        private void btnN2_Click(object sender, EventArgs e)
+        {
+            tbxPantalla.Text = tbxPantalla.Text + "2";
+
+        }
+        private void btnN3_Click(object sender, EventArgs e)
+        {
+            tbxPantalla.Text = tbxPantalla.Text + "3";
+
+        }
+        private void btnN4_Click(object sender, EventArgs e)
+        {
+            tbxPantalla.Text = tbxPantalla.Text + "4";
+
+        }
+        private void btnN5_Click(object sender, EventArgs e)
+        {
+            tbxPantalla.Text = tbxPantalla.Text + "5";
+
+        }
+        private void btnN6_Click(object sender, EventArgs e)
+        {
+            tbxPantalla.Text = tbxPantalla.Text + "6";
+
+        }
+        private void btnN7_Click(object sender, EventArgs e)
+        {
+            tbxPantalla.Text = tbxPantalla.Text + "7";
+
+        }
+        private void btnN8_Click(object sender, EventArgs e)
+        {
+            tbxPantalla.Text = tbxPantalla.Text + "8";
+
+        }
+        private void btnN9_Click(object sender, EventArgs e)
+        {
+            tbxPantalla.Text = tbxPantalla.Text + "9";
+
+        }
+
+       
+    }
+}
